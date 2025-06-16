d3a666e4fa0009d961a767febd988603f275988b
shortiwsky <tomas.short1@gmail.com>
2025-06-16 23:46:19 +0100

    carrinho com botao de remover produtos

--- scripts/carrinho.js
+++ scripts/carrinho.js
@@ -1,7 +1,7 @@
 function carregarCarrinho() {
   const carrinho = JSON.parse(localStorage.getItem("carrinho")) || [];
   const container = document.querySelector(".form2-box");
-  container.innerHTML = ""; // limpar antes de inserir
+  container.innerHTML = "";
   let total = 0;
 
   carrinho.forEach((produto, index) => {
@@ -9,25 +9,32 @@ function carregarCarrinho() {
     const subtotal = (preco * produto.quantidade).toFixed(2);
     total += parseFloat(subtotal);
 
-    const produtoHTML = `
-      <div class="produto">
-        <img src="${produto.imagem}" alt="${produto.nome}">
-        <div class="produto-info">
-          <div class="produto-nome">${produto.nome}</div>
-        </div>
-        <input class="quantidade" type="number" value="${produto.quantidade}" min="1" onchange="atualizarQuantidade(${index}, this.value)" />
-        <div class="subtotal">${subtotal}€</div>
-        <button class="remover-btn" onclick="removerProduto(${index})">Remover</button>
-      </div>
-    `;
+  const produtoHTML = `
+  <div class="item-carrinho">
+    <button class="remover-btn" onclick="removerProduto(${index})" title="Remover produto">
+      <svg xmlns="http://www.w3.org/2000/svg" class="icon-lixo" viewBox="0 0 24 24" fill="currentColor">
+        <path d="M9 3a1 1 0 00-1 1v1H5.5a.5.5 0 000 1H6v13a2 2 0 002 2h8a2 2 0 002-2V6h.5a.5.5 0 000-1H16V4a1 1 0 00-1-1H9zm1 4a.5.5 0 011 0v10a.5.5 0 01-1 0V7zm4 0a.5.5 0 011 0v10a.5.5 0 01-1 0V7z"/>
+      </svg>
+    </button>
+    <img class="item-img" src="${produto.imagem}" alt="${produto.nome}">
+    <div class="item-info">
+      <h3 class="item-nome">${produto.nome}</h3>
+      <p class="item-preco">${produto.preco}</p>
+    </div>
+    <input class="item-quantidade" type="number" value="${produto.quantidade}" min="1"
+           onchange="atualizarQuantidade(${index}, this.value)" />
+    <div class="item-subtotal">${subtotal}€</div>
+  </div>
+`;
+
 
     container.insertAdjacentHTML("beforeend", produtoHTML);
   });
 
   container.insertAdjacentHTML("beforeend", `
-    <div class="total">
-      Total: <strong>${total.toFixed(2)}€</strong><br>
-      <a href="compra.html" class="finalizar-btn">Comprar</a>
+    <div class="carrinho-total">
+      <strong>Total: ${total.toFixed(2)}€</strong>
+      <a href="compra.html" class="finalizar-btn">Finalizar Compra</a>
     </div>
   `);
 }
@@ -37,18 +44,15 @@ function atualizarQuantidade(index, novaQuantidade) {
   if (carrinho[index]) {
     carrinho[index].quantidade = parseInt(novaQuantidade);
     localStorage.setItem("carrinho", JSON.stringify(carrinho));
-    location.reload(); // Atualiza a página
+    location.reload();
   }
 }
 
 function removerProduto(index) {
   let carrinho = JSON.parse(localStorage.getItem("carrinho")) || [];
-  if (carrinho[index]) {
-    carrinho.splice(index, 1); // remove 1 item na posição index
-    localStorage.setItem("carrinho", JSON.stringify(carrinho));
-    location.reload(); // Atualiza a página
-  }
+  carrinho.splice(index, 1);
+  localStorage.setItem("carrinho", JSON.stringify(carrinho));
+  location.reload();
 }
 
 carregarCarrinho();
-
